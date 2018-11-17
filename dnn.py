0c45cd357c83cacdd8d84154a3ac838f7ad93a73
unknown <davibeltrao02@gmail.com>
2018-11-17 21:43:12 -0200

    Used StandardScaler to scale the data. Model conververging. Need to test different parameters

--- dnn.py
+++ dnn.py
@@ -1,4 +1,64 @@
 import parse_data as data_loader
+from keras.layers import Input, Dense, Activation
+from keras.models import Model
+from keras.optimizers import SGD
+import keras
+import time
 
 
-data_loader.load_data_kfold(10)
+def get_model(inp, n_classes):
+    activation = 'relu'
+    
+    H = Dense(64)(inp)
+    H = Activation(activation)(H)
+    # H = Dense(32, activation=activation)(H)
+    # H = Dense(16, activation=activation)(H)
+    H = Dense(n_classes)(H)
+    out = Activation('softmax')(H)
+
+    model = Model(inp, out)
+    
+    opt = keras.optimizers.Adagrad()
+
+    model.compile(optimizer=opt,
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    return model
+
+
+folds, X_data, y_data = data_loader.load_data_kfold(10)
+print(X_data.shape)
+
+
+x = Input(shape=(X_data.shape[1],))
+model = get_model(inp=x, n_classes=3)
+
+for j, (train_idx, val_idx) in enumerate(folds):
+    print('\nFold ', j)
+    X_train_cv = X_data[train_idx]
+    y_train_cv = y_data[train_idx]
+    X_valid_cv = X_data[val_idx]
+    y_valid_cv = y_data[val_idx]
+
+    print("X_train: {}".format(X_train_cv))
+    print("y_train: {}".format(y_train_cv))
+
+    # time.sleep(5)
+
+    print("X_validation: {}".format(X_valid_cv.shape))
+    print("y_validation: {}".format(y_valid_cv.shape))
+
+    x = Input(shape=(X_train_cv.shape[1],))
+    model = get_model(inp=x, n_classes=3)
+
+    model.fit(X_train_cv,
+              y_train_cv,
+              epochs=200,
+              verbose=1,
+              validation_data=(X_valid_cv, y_valid_cv)
+              )
+
+    del model
+
+    print(x)
--- parse_data.py
+++ parse_data.py
@@ -2,22 +2,40 @@ import csv
 import numpy as np
 from numpy import genfromtxt
 from sklearn.model_selection import train_test_split, StratifiedKFold
+import pandas as pd
+from sklearn import preprocessing
+from keras.utils import to_categorical
+from sklearn.preprocessing import StandardScaler
+import time
 
 
 def load_data_kfold(k):
-    data = genfromtxt('sdss.csv', delimiter=',')
-    data = data[1:]
-    print(data)
-
-    X_data = data[:, :-1]
-    y_data = data[:, -1]
-
+    df = pd.read_csv('sdss.csv', sep=',')
+    
+    y_data = df['class']
+    print(y_data.head(10))
+    del df['class']
+    X_data = np.array(df)
+    # X_data = df
     print(X_data.shape)
-    print(y_data.reshape(-1, 1).shape)
+
+    scaler = StandardScaler()
+    X_data = scaler.fit_transform(X_data)
+    # print(X_data.head(5))
+    # print(scaled_data)
 
     folds = list(StratifiedKFold(n_splits=k, shuffle=True, random_state=1)
                  .split(X_data, y_data))
 
     folds = np.array(folds)
+
+    label_encoder = preprocessing.LabelEncoder()
+    y_data = label_encoder.fit_transform(y_data)
+    y_data = to_categorical(y_data, 3)
+    
+    print(folds)
+    # print(y_data.reshape(-1, 1))
+    print("Y data")
+    print(y_data.shape)
 
     return folds, X_data, y_data
