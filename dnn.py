980a8e941d9325b31d51da5761323dca22123a85
davi <davibeltrao02@gmail.com>
2018-12-03 00:33:37 -0200

    Model updated

--- dnn.py
+++ dnn.py
@@ -4,12 +4,27 @@ from keras.models import Model
 from keras.optimizers import SGD
 import keras
 import time
+import numpy as np
+from sklearn.metrics.classification import accuracy_score, recall_score, f1_score
+from sklearn.model_selection import train_test_split
 
 
 def get_model(inp, n_classes):
     activation = 'relu'
     
     H = Dense(64)(inp)
+    H = Activation(activation)(H)
+
+    H = Dense(64)(H)
+    H = Activation(activation)(H)
+
+    H = Dense(64)(H)
+    H = Activation(activation)(H)
+
+    H = Dense(64)(H)
+    H = Activation(activation)(H)
+
+    H = Dense(64)(H)
     H = Activation(activation)(H)
     # H = Dense(32, activation=activation)(H)
     # H = Dense(16, activation=activation)(H)
@@ -18,7 +33,7 @@ def get_model(inp, n_classes):
 
     model = Model(inp, out)
     
-    opt = keras.optimizers.Adagrad()
+    opt = keras.optimizers.Adadelta()
 
     model.compile(optimizer=opt,
                   loss='categorical_crossentropy',
@@ -30,35 +45,41 @@ def get_model(inp, n_classes):
 folds, X_data, y_data = data_loader.load_data_kfold(10)
 print(X_data.shape)
 
-
 x = Input(shape=(X_data.shape[1],))
 model = get_model(inp=x, n_classes=3)
 
-for j, (train_idx, val_idx) in enumerate(folds):
+for j, (train_idx, test_idx) in enumerate(folds):
     print('\nFold ', j)
     X_train_cv = X_data[train_idx]
     y_train_cv = y_data[train_idx]
-    X_valid_cv = X_data[val_idx]
-    y_valid_cv = y_data[val_idx]
 
-    print("X_train: {}".format(X_train_cv))
-    print("y_train: {}".format(y_train_cv))
+    X_train, X_test, y_train, y_test = train_test_split(X_train_cv,
+                                                        y_train_cv,
+                                                        test_size=0.33,
+                                                        random_state=42
+                                                        )
 
-    # time.sleep(5)
-
-    print("X_validation: {}".format(X_valid_cv.shape))
-    print("y_validation: {}".format(y_valid_cv.shape))
+    X_test_cv = X_data[test_idx]
+    y_test_cv = y_data[test_idx]
 
     x = Input(shape=(X_train_cv.shape[1],))
     model = get_model(inp=x, n_classes=3)
 
-    model.fit(X_train_cv,
-              y_train_cv,
-              epochs=200,
+    model.fit(X_train,
+              y_train,
+              epochs=80,
               verbose=1,
-              validation_data=(X_valid_cv, y_valid_cv)
+              validation_data=(X_test, y_test)
               )
 
+    y_pred = model.predict(X_test_cv)
+    y_pred = np.argmax(y_pred, axis=1)
+
+    y_true = np.argmax(y_test_cv, axis=1)
+
+    acc_fold = accuracy_score(y_true, y_pred)
+    print("\n\n Accuracy: {}".format(acc_fold))
+    time.sleep(2)
     del model
 
     print(x)
